3e127ff7adf5c88765cbdc940fa393ad7973ed79
nimofarah <faranim01@gmail.com>
2024-10-10 12:42:01 +0300

    tehty

--- src/main/java/TimeCalculator.java
+++ src/main/java/TimeCalculator.java
@@ -1,5 +1,10 @@
 
 public class TimeCalculator {
+    public static void main(String[] args) {
+
+        System.out.println("1+1=2");
+
+    }
     public double calculateTime(double distance, double speed){
         if (speed <= 0){
             throw new IllegalArgumentException("Speed must be greater than 0");
